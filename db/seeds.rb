daa8d28860d5a25f5c3fef245f5e23d3b31b1f78
PH Denain <phdenain@me.com>
2019-11-13 18:42:55 +0100

    flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!([{
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: 'Lovely Flat in Chelsea',
+  address: '39 Royal Avenue London SW3 4QE',
+  description: 'Clear Views, brilliant location',
+  price_per_night: 300,
+  number_of_guests: 4
+  },
+  {
+  name: 'Stunning house near Hyde Park',
+  address: '3 Douro Place, London W8 5PH',
+  description: 'Roomy, quiet, great location',
+  price_per_night: 500,
+  number_of_guests: 6
+  }
+])
+
+
